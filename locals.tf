05e4a7dffe4f73139fec0099d5ef42b9c3246e1d
David Fischer <david@fisch3r.net>
2025-03-03 20:29:59 +0100

    Set pages_external_url to https, ensure GitLab Pages is enabled

--- locals.tf
+++ locals.tf
@@ -8,10 +8,11 @@ locals {
     "gitlab_shell['log_level'] = '${upper(var.log_level)}'",
 
     # https://docs.gitlab.com/administration/pages/#access-control
+    "gitlab_pages['enabled'] = true",
     "gitlab_pages['access_control'] = true",
 
     # https://gitlab.fisch3r.net/help/administration/pages/index.md
-    "pages_external_url 'http://${var.pages_domain}'",
+    "pages_external_url 'https://${var.pages_domain}'",
     "pages_nginx['enabled'] = true",
     "pages_nginx['listen_port'] = 80",
     "pages_nginx['listen_https'] = false",
